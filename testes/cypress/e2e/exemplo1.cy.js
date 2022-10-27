0aad45531f7f9aea6de5108bfd5a50b5285e102d
Henriques100 <110470168+Henriques100@users.noreply.github.com>
2022-10-26 23:57:05 -0300

    testes

--- testes/cypress/e2e/exemplo1.cy.js
+++ testes/cypress/e2e/exemplo1.cy.js
@@ -1,5 +1,10 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+/// <reference> types="cypress"/>
+
+describe('Criando cenário de teste ao site globalsqa',()=> {
+
+  it('Caso de teste: Registrando um usuário no site com sucesso', ()=> {
+    
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+    cy.get('.btn-link').click()
   })
 })
